2ae8e3b5f58d15657b942f65dba2120f61cb95c6
Nisi06 <denise.kumpfmueller@web.de>
2023-10-01 20:23:02 +0200

    added the first function for the choices

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
-// 1 == Rock
-// 2 == Paper
-// 3 == Scissors
 
-function getComputerChoice() {
+function computerChoice() {
     let choices = ["Rock" , "Paper" , "Scissors"];
     let compChoice = choices[Math.floor(Math.random() * 3)];
-    return compChoice;
+    return compChoice.toUpperCase();
 }
 
-console.log(getComputerChoice);
+
+function playerChoice() {
+    let yourChoice = prompt("Please choose 'Rock' or 'Paper' or 'Scissors'.");
+    return yourChoice.toUpperCase();
+}
+
+
+let computerSelection = computerChoice();
+let playerSelection = playerChoice();
+
+
+function game(playerSelection, computerSelection){
+
+    if(computerSelection == playerSelection) {
+        return "It's a tie!";
+    } else if(
+        (computerSelection == "ROCK" && playerSelection == "SCISSORS")
+        || (computerSelection == "PAPER" && playerSelection == "ROCK")
+        || (computerSelection() == "SCISSORS" && playerSelection == "PAPER")) {
+            return `You lost! ${computerSelection} beats ${playerSelection}!`;
+    } else {
+        return `You win! ${playerSelection} beats ${computerSelection}!`
+    }
+}
+
+
+let computerScore = 0;
+let playerScore = 0;
+
+console.log(game(playerSelection, computerSelection));
+
+
+
